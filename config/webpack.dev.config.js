6e79b4f6763d98541643fdc16ede95a9a7a6fc9a
Roberto <rob.vicalvaro@gmail.com>
2019-03-24 00:22:31 +0100

    improvements
    prettierrc for config singlequotes accepted, convert const to import in webpack.prod.config.js and webpack.dev.config.js

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,18 +1,18 @@
-const webpack = require("webpack");
-const path = require("path");
-const merge = require("webpack-merge");
-const common = require("../webpack.config");
+import webpack from "webpack";
+import path from "path";
+import merge from "webpack-merge";
+import common from "../webpack.config";
 
 const hotModulePlugin = new webpack.HotModuleReplacementPlugin();
 
 module.exports = merge(common, {
-    mode: "development",
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: false,
-        port: 3000,
-        hot: true
-    },
-    devtool: 'source-map',
-    plugins: [hotModulePlugin]
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: false,
+    port: 3000,
+    hot: true
+  },
+  devtool: "source-map",
+  plugins: [hotModulePlugin]
 });
--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,39 +1,28 @@
-const webpack = require("webpack");
-const merge = require("webpack-merge");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const OptimizeCSSPlugin = require("optimize-css-assets-webpack-plugin");
-const common = require("../webpack.config");
-
+import merge from "webpack-merge";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import CleanWebpackPlugin from "clean-webpack-plugin";
+import OptimizeCSSPlugin from "optimize-css-assets-webpack-plugin";
+import common from "../webpack.config";
 
 const cleanPlugin = new CleanWebpackPlugin();
 const cssMiniPlugin = new MiniCssExtractPlugin({
-    filename: "[name].css",
-    chunkFilename: "[hash].css"
+  filename: "[name].css",
+  chunkFilename: "[hash].css"
 });
 
 module.exports = merge(common, {
-    mode: "production",
-    devtool: false,
-    plugins: [
-        cleanPlugin,
-        cssMiniPlugin
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(sc|c)ss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader"
-                ]
-            }
-        ]
-    },
-    optimization: {
-        minimizer: [
-            new OptimizeCSSPlugin()
-        ]
-    }
+  mode: "production",
+  devtool: false,
+  plugins: [cleanPlugin, cssMiniPlugin],
+  module: {
+    rules: [
+      {
+        test: /\.(sc|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [new OptimizeCSSPlugin()]
+  }
 });
